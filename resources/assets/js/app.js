96b5740d65e57945a0c762f3a65be608b01f161c
Binh <ttbinh@hueic.edu.vn>
2018-07-26 16:21:01 +0700

    Finish first step task

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -20,3 +20,8 @@ Vue.component('example-component', require('./components/ExampleComponent.vue'))
 const app = new Vue({
     el: '#app'
 });
+
+function logout(){
+    event.preventDefault();
+    document.getElementById('logout-form').submit();
+}
--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -16,4 +16,13 @@ return [
     'failed' => 'These credentials do not match our records.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
+    'login' => 'Login',
+    'email' => 'E-Mail Address',
+    'password' => 'Password',
+    'rememberMe' => 'Remember Me',
+    'forgotYourPassword' => 'Forgot Your Password?',
+    'resetPassword' => 'Reset Password',
+    'passwordConfirm' => 'Confirm Password',
+    'sendPasswordResetLink' => 'Send Password Reset Link',
+
 ];
